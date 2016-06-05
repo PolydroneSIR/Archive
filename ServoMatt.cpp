6b13eee7afac80cacb48f947a1225c311f57c76b
PolydroneSIR <matthieulaneyrie@hotmail.com>
2016-06-05 23:50:07 +0200

    Update ServoMatt.cpp

--- ServoMatt.cpp
+++ ServoMatt.cpp
@@ -1,15 +1,4 @@
 /*
-Provided to you by Emlid Ltd (c) 2014.
-twitter.com/emlidtech || www.emlid.com || info@emlid.com
-
-Example: Control servos connected to PCA9685 driver onboard of Navio shield for Raspberry Pi.
-
-Connect servo to Navio's rc output and watch it work.
-Output 1 on board is connected to PCA9685 channel 3, output 2 to channel 4 and so on.
-To use full range of your servo correct SERVO_MIN and SERVO_MAX according to it's specification.
-
-To run this example navigate to the directory containing it and run following commands:
-make
 ./Servo
 */
 
